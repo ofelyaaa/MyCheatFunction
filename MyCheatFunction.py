c1a95a141486a7c4de04c484b4885e5d173b5c54
ofelyaaa <157810991+ofelyaaa@users.noreply.github.com>
2024-01-26 15:34:40 +0100

    Update MyCheatFunction.py

--- MyCheatFunction.py
+++ MyCheatFunction.py
@@ -2,9 +2,18 @@ import numpy as np
 import pandas as pd
 
 def cheat(question_number):
-  if question_number == "Q1.2P.6":
-    sample_scores = np.array([1, 6, 7, 8, 9, np.nan])
-    print(np.nvar(sample_scores))
+  if question_number == "Q1.2P.9":
+    np.random.seed(1234)  # Set the random seed
+    speed_sec = np.zeros(10)
+    sim_speed = np.random.uniform(size=5)  # Speed simulation in seconds
+    speed_sec[0:5] = sim_speed * np.random.uniform(low=0.5, high=10, size=5)
+    speed_sec[5:10] = sim_speed
+
+    d = {'language': list(['R'] * 5 + ['Python'] * 5),
+         'codetype': list([f'forloop{i}' for i in range(1, 6)] * 2),
+         'speed': speed_sec}
+    simmydf = pd.DataFrame.from_dict(d)
+    print(simmydf)
   if question_number == "Q1.2P.7":
     myarray = np.full([4,3,5],np.nan)
     print(myarray)
